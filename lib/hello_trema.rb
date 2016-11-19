bd29882302e54be246ec4cf81bfe3e1b5d9538c5
ALiCEjAUNE <buttyoseijin@gmail.com>
2016-11-20 04:30:45 +0900

    Masterには完成品だけ置いていきます developブランチ切ったので以降開発はそちら

--- lib/hello_trema.rb
+++
@@ -1,9 +0,0 @@
-class HelloTrema < Trema::Controller
-  def start(_args)
-    logger.info 'Trema started.'
-  end
-
-  def switch_ready(datapath_id)
-    logger.info "Hello #{datapath_id.to_hex}!"
-  end
-end
